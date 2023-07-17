51bf7f00b6218da037e3feee82ee29c0f5bd194e
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-16 21:20:28 -0700

    Test passed changing from one bread to five.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,23 +9,22 @@ namespace Bakery.Tests
         [TestMethod]
         public void BreadConstructor_CreatesInstanceOfBreadClass_Bread()
         {
-            Bread newBread = new Bread(1);
+            Bread newBread = new Bread(5);
             Assert.AreEqual(typeof(Bread), newBread.GetType());
         }
         [TestMethod]
         public void GetHowManyBreads_ReturnsHowManyBreads_Int()
         {
-          int count = 1;
-          Bread newBread = new Bread(1);
+          Bread newBread = new Bread(5);
           int result = newBread.HowManyBreads;
-          Assert.AreEqual(1, result);
+          Assert.AreEqual(5, result);
         }
         [TestMethod]
         public void BreadPrice_ReturnsBreadPrice_Int()
         {
-          Bread newBread = new Bread(1);
+          Bread newBread = new Bread(5);
           int result = newBread.BreadPrice();
-          Assert.AreEqual(5, result);
+          Assert.AreEqual(25, result);
         }
     }
 }
