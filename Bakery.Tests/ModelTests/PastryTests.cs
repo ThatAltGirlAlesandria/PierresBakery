7348ba856b9f3633c9b7791e5dade45610d36e7f
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-16 20:29:36 -0700

    Pastry constructor now passing though tests.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,14 +1,13 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
-using System.Collections.Generic;
-using System;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class PastryTests
   {
-    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastryClass_Pastry()
     {
       Pastry newPastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
